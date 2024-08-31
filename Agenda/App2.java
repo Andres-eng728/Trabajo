8d6713226bb018a870d8340e99f20013b409b6ea
Santiago-Bayona <Cesars.bayonap@uqvirtual.edu.co>
2024-08-31 14:15:57 -0500

    Add files via upload

---
+++ Agenda/App2.java
@@ -0,0 +1,6 @@
+package Agenda;
+
+public class App2 {
+
+    Contacto contacto = new Contacto("Jose","Lks","Armenia","3251145","fgehgegd@gmail.com");
+}
---
+++ Agenda/Contacto.java
@@ -0,0 +1,63 @@
+package Agenda;
+
+public class Contacto {
+    public String Nombre;
+    public String Alias;
+    public String Direccion;
+    public String Telefono;
+    public String Email;
+
+    public Contacto(String Nombre,String Alias, String Direccion, String Telefono, String Email){
+        this.Nombre=Nombre;
+        this.Alias=Alias;
+        this.Direccion=Direccion;
+        this.Telefono=Telefono;
+        this.Email=Email;
+    }
+
+    public String getNombre(){
+        return Nombre;
+    }
+    public void setNombre(String Nombre){
+        this.Nombre=Nombre;
+    }
+
+    public String getAlias(){
+        return Alias;
+    }
+    public void setAlias(String Alias){
+        this.Alias=Alias;
+    }
+
+    public String getDireccion(){
+        return Direccion;
+    }
+    public void setDireccion(String Direccion){
+        this.Direccion=Direccion;
+    }
+
+    public String getTelefono(){
+        return Telefono;
+    }
+    public void setTelefono(String Telefono){
+        this.Telefono=Telefono;
+    }
+
+    public String getEmail(){
+        return Email;
+    }
+    public void setEmail(String Email){
+        this.Email=Email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "Nombre='" + Nombre + '\'' +
+                ", Alias='" + Alias + '\'' +
+                ", Direccion='" + Direccion + '\'' +
+                ", Telefono='" + Telefono + '\'' +
+                ", Email='" + Email + '\'' +
+                '}';
+    }
+}
